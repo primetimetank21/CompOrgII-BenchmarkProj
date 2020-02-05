59b34191eb8c5316a069dcb4566a40d446cac165
Earl Tankard, Jr <theflashtank21@gmail.com>
2020-02-05 00:36:30 -0500

    Program now writes to a file AFTER ALL BENCHMARKS ARE COMPLETE

--- main.cpp
+++ main.cpp
@@ -14,12 +14,8 @@ using namespace std;
 
 // Global Variable Declarations
 typedef chrono::high_resolution_clock Clock;
-//double int_microseconds       = NULL;
-//double float_microseconds     = NULL;
-//double memory_microseconds    = NULL;
-//double hardDrive_microseconds = NULL;
 
-// Function Declaration
+// Function Declarations
 double intOpBenchmark();
 double floatOpBenchmark();
 double memoryOpBenchmark();
@@ -27,76 +23,104 @@ double hardDriveOpBenchmark();
 
 // Main
 int main() {
+    // Variables to store results of each benchmark
     double int_microseconds       = intOpBenchmark();
     double float_microseconds     = floatOpBenchmark();
     double memory_microseconds    = memoryOpBenchmark();
     double hardDrive_microseconds = hardDriveOpBenchmark();
 
+    // Output each result to console
     cout << "32-bit Integer operation benchmark:\t\t\t"    << int_microseconds       << " microseconds\n";
     cout << "32-bit Floating point operation benchmark:\t" << float_microseconds     << " microseconds\n";
     cout << "Memory benchmark:\t\t\t\t\t\t\t"              << memory_microseconds    << " microseconds\n";
     cout << "Hard drive benchmark:\t\t\t\t\t\t"            << hardDrive_microseconds << " microseconds\n";
+    
+    // Write results to a file
+    ofstream results;
+    results.open("/Users/earltankardjr/comporgii/benchmark_project/benchmarkmaker.cpp/benchmarkmaker.cpp/benchmark_results.txt");
+    
+    results << "Benchmark Results:\n";
+    results << "32-bit Integer operation benchmark:\t\t"      << int_microseconds       << " microseconds\n"
+            << "32-bit Floating point operation benchmark:\t" << float_microseconds     << " microseconds\n"
+            << "Memory benchmark:\t\t\t\t"                    << memory_microseconds    << " microseconds\n"
+            << "Hard drive benchmark:\t\t\t\t"                << hardDrive_microseconds << " microseconds\n";
+    results.close();
     return 0;
 }
 
-// // // // // // // // // // // // // // // // // // // // // // // //
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // Function Definitions // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+
 double intOpBenchmark() {
-    auto int_t1 = Clock::now();
     const unsigned int n = 2;
+    auto int_t1 = Clock::now();         // starts timer
     
     for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}   // 10^11 additions
-    for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}   // 5 * 10^10 multiplication
-    for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}   // 2 * 10^10 division
+    for(unsigned long long int i = 0; i < pow(10,10) * 5; i++) {n*n;}   // 5 * 10^10 multiplication
+    for(unsigned long long int i = 0; i < pow(10,10) * 2; i++) {n/n;}   // 2 * 10^10 division
     
-    auto int_t2 = Clock::now();
+    auto int_t2 = Clock::now();         // ends timer
 
-    return std::chrono::duration_cast<std::chrono::microseconds>(int_t2 - int_t1).count();
+    return chrono::duration_cast<chrono::microseconds>(int_t2 - int_t1).count();
 }
-// // // // // // // // // // // // // // // // // // // // // // // //
-
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 double floatOpBenchmark() {
-   auto float_t1 = Clock::now();
-   const float n = 2;
+    const float n = 2;
+    auto float_t1 = Clock::now();       // starts timer
    
-   for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}    // 10^11 additions
-   for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}    // 5 * 10^10 multiplication
-   for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}    // 2 * 10^10 division
+    for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}    // 10^11 additions
+    for(unsigned long long int i = 0; i < pow(10,10) * 5; i++) {n*n;}    // 5 * 10^10 multiplication
+    for(unsigned long long int i = 0; i < pow(10,10) * 2; i++) {n/n;}    // 2 * 10^10 division
    
-   auto float_t2 = Clock::now();
+    auto float_t2 = Clock::now();       // ends timer
     
-   return std::chrono::duration_cast<std::chrono::microseconds>(float_t2 - float_t1).count();
+    return chrono::duration_cast<chrono::microseconds>(float_t2 - float_t1).count();
 }
-// // // // // // // // // // // // // // // // // // // // // // // //
-
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 double memoryOpBenchmark() {
-    auto memory_t1 = Clock::now();
     const long long int _size = 5 * pow(10,9);
     long int array[_size];
+    auto memory_t1 = Clock::now();      // starts timer
+
     
     for(unsigned long long int i = 0; i < _size; i++) {array[i] = 1;}   // 5 * 10^9 writes of elements
     for(long int i : array)                           {i;}              // 5 * 10^9 reads  of elements
     
-    auto memory_t2 = Clock::now();
+    auto memory_t2 = Clock::now();      // ends timer
     
-    return std::chrono::duration_cast<std::chrono::microseconds>(memory_t2 - memory_t1).count();
+    return chrono::duration_cast<chrono::microseconds>(memory_t2 - memory_t1).count();
 }
-// // // // // // // // // // // // // // // // // // // // // // // //
-
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 double hardDriveOpBenchmark() {
-    auto hardDrive_t1 = Clock::now();
     ifstream readfile;
     ofstream writefile;
-    unsigned long long int num;
+    char chars[4];
+    auto hardDrive_t1 = Clock::now();       // starts timer
     
     writefile.open("/Users/earltankardjr/comporgii/benchmark_project/benchmarkmaker.cpp/benchmarkmaker.cpp/benchmark.txt");
-    for(unsigned long long int i = 0; i < pow(10,9); i++) {writefile << i << ' ';}  // 10^9 writes to a file
+    for(unsigned long int i = 0; i < pow(10,9); i++) {
+        writefile << 'a'<< 'b'<< 'c'<< ' ';     // 10^9 writes to a file (4 BYTES AT A TIME)
+    }
     writefile.close();
     
     readfile.open("/Users/earltankardjr/comporgii/benchmark_project/benchmarkmaker.cpp/benchmarkmaker.cpp/benchmark.txt");
-    for(unsigned long long int i = 0; i < pow(10,9); i++) {readfile >> num;}        // 10^9 reads from a file
+    for(unsigned long int i = 0; i < pow(10,9); i++) {
+        readfile >> chars;      // 10^9 reads from a file (even though chars == 3 bytes, program still reads the space; therefore the 4th byte is being read)
+    }
     readfile.close();
     
-    auto hardDrive_t2 = Clock::now();
+    auto hardDrive_t2 = Clock::now();       // ends timer
     
-    return std::chrono::duration_cast<std::chrono::microseconds>(hardDrive_t2 - hardDrive_t1).count();
+    return chrono::duration_cast<chrono::microseconds>(hardDrive_t2 - hardDrive_t1).count();
 }
