ef94b9c3e24f765eeb847eb0ae1be8ba703ec44b
Earl Tankard, Jr <theflashtank21@gmail.com>
2020-02-04 22:39:22 -0500

    Implemented the Memory benchmark; working on Hard drive benchmark

--- main.cpp
+++ main.cpp
@@ -28,12 +28,14 @@ void hardDriveOpBenchmark();
 int main() {
     intOpBenchmark();
     floatOpBenchmark();
-//    memoryOpBenchmark();
+    memoryOpBenchmark();
 //    hardDriveOpBenchmark();
     cout << "32-bit Integer operation benchmark: "
          << int_microseconds   << " microseconds\n";
     cout << "32-bit Floating point operation benchmark: "
          << float_microseconds << " microseconds\n";
+    cout << "Memory benchmark: "
+         << memory_microseconds << " microseconds\n";
     return 0;
 }
 
@@ -42,9 +44,9 @@ void intOpBenchmark() {
     auto int_t1 = Clock::now();
     const unsigned int n = 2;
     
-    for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}
-    for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}
-    for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}
+    for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}   // 10^11 additions
+    for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}   // 5 * 10^10 multiplication
+    for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}   // 2 * 10^10 division
     
     auto int_t2 = Clock::now();
 
@@ -56,11 +58,24 @@ void floatOpBenchmark() {
    auto float_t1 = Clock::now();
    const float n = 2;
    
-   for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}
-   for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}
-   for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}
+   for(unsigned long long int i = 0; i < pow(10,11)    ; i++) {n+n;}    // 10^11 additions
+   for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}    // 5 * 10^10 multiplication
+   for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}    // 2 * 10^10 division
    
    auto float_t2 = Clock::now();
     
    float_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(float_t2 - float_t1).count();
+}
+// // // // // // // // // // // // // // // // // // // // // // // //
+
+void memoryOpBenchmark() {
+    auto memory_t1 = Clock::now();
+    const long long int _size = 5 * pow(10,9);
+    long int array[_size];
+    
+    for(unsigned long long int i = 0; i < _size; i++) {array[i] = 1;}   // 5 * 10^9 writes of elements
+    for(long int i : array)                           {i;}              // 5 * 10^9 reads  of elements
+    
+    auto memory_t2 = Clock::now();
+    memory_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(memory_t2 - memory_t1).count();
 }
