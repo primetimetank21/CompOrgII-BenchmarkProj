8e7091b74fb55b25c6f637e0fa3ad3ffd108cfcc
Earl Tankard, Jr <theflashtank21@gmail.com>
2020-02-04 23:40:35 -0500

    Implemented Hard drive benchmark; starting to clean up/organize code before running full benchmark

--- main.cpp
+++ main.cpp
@@ -7,40 +7,40 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include <chrono>
 using namespace std;
 
 // Global Variable Declarations
 typedef chrono::high_resolution_clock Clock;
-float int_microseconds       = NULL;
-float float_microseconds     = NULL;
-float memory_microseconds    = NULL;
-float hardDrive_microseconds = NULL;
+//double int_microseconds       = NULL;
+//double float_microseconds     = NULL;
+//double memory_microseconds    = NULL;
+//double hardDrive_microseconds = NULL;
 
 // Function Declaration
-void intOpBenchmark();
-void floatOpBenchmark();
-void memoryOpBenchmark();
-void hardDriveOpBenchmark();
+double intOpBenchmark();
+double floatOpBenchmark();
+double memoryOpBenchmark();
+double hardDriveOpBenchmark();
 
 // Main
 int main() {
-    intOpBenchmark();
-    floatOpBenchmark();
-    memoryOpBenchmark();
-//    hardDriveOpBenchmark();
-    cout << "32-bit Integer operation benchmark: "
-         << int_microseconds   << " microseconds\n";
-    cout << "32-bit Floating point operation benchmark: "
-         << float_microseconds << " microseconds\n";
-    cout << "Memory benchmark: "
-         << memory_microseconds << " microseconds\n";
+    double int_microseconds       = intOpBenchmark();
+    double float_microseconds     = floatOpBenchmark();
+    double memory_microseconds    = memoryOpBenchmark();
+    double hardDrive_microseconds = hardDriveOpBenchmark();
+
+    cout << "32-bit Integer operation benchmark:\t\t\t"    << int_microseconds       << " microseconds\n";
+    cout << "32-bit Floating point operation benchmark:\t" << float_microseconds     << " microseconds\n";
+    cout << "Memory benchmark:\t\t\t\t\t\t\t"              << memory_microseconds    << " microseconds\n";
+    cout << "Hard drive benchmark:\t\t\t\t\t\t"            << hardDrive_microseconds << " microseconds\n";
     return 0;
 }
 
 // // // // // // // // // // // // // // // // // // // // // // // //
-void intOpBenchmark() {
+double intOpBenchmark() {
     auto int_t1 = Clock::now();
     const unsigned int n = 2;
     
@@ -50,11 +50,11 @@ void intOpBenchmark() {
     
     auto int_t2 = Clock::now();
 
-    int_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(int_t2 - int_t1).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(int_t2 - int_t1).count();
 }
 // // // // // // // // // // // // // // // // // // // // // // // //
 
-void floatOpBenchmark() {
+double floatOpBenchmark() {
    auto float_t1 = Clock::now();
    const float n = 2;
    
@@ -64,11 +64,11 @@ void floatOpBenchmark() {
    
    auto float_t2 = Clock::now();
     
-   float_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(float_t2 - float_t1).count();
+   return std::chrono::duration_cast<std::chrono::microseconds>(float_t2 - float_t1).count();
 }
 // // // // // // // // // // // // // // // // // // // // // // // //
 
-void memoryOpBenchmark() {
+double memoryOpBenchmark() {
     auto memory_t1 = Clock::now();
     const long long int _size = 5 * pow(10,9);
     long int array[_size];
@@ -77,5 +77,26 @@ void memoryOpBenchmark() {
     for(long int i : array)                           {i;}              // 5 * 10^9 reads  of elements
     
     auto memory_t2 = Clock::now();
-    memory_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(memory_t2 - memory_t1).count();
+    
+    return std::chrono::duration_cast<std::chrono::microseconds>(memory_t2 - memory_t1).count();
+}
+// // // // // // // // // // // // // // // // // // // // // // // //
+
+double hardDriveOpBenchmark() {
+    auto hardDrive_t1 = Clock::now();
+    ifstream readfile;
+    ofstream writefile;
+    unsigned long long int num;
+    
+    writefile.open("/Users/earltankardjr/comporgii/benchmark_project/benchmarkmaker.cpp/benchmarkmaker.cpp/benchmark.txt");
+    for(unsigned long long int i = 0; i < pow(10,9); i++) {writefile << i << ' ';}  // 10^9 writes to a file
+    writefile.close();
+    
+    readfile.open("/Users/earltankardjr/comporgii/benchmark_project/benchmarkmaker.cpp/benchmarkmaker.cpp/benchmark.txt");
+    for(unsigned long long int i = 0; i < pow(10,9); i++) {readfile >> num;}        // 10^9 reads from a file
+    readfile.close();
+    
+    auto hardDrive_t2 = Clock::now();
+    
+    return std::chrono::duration_cast<std::chrono::microseconds>(hardDrive_t2 - hardDrive_t1).count();
 }
