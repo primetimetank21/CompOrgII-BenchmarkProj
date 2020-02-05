52f65186df726254ff417ba4074d7438143c491a
Earl Tankard, Jr <theflashtank21@gmail.com>
2020-02-04 22:01:04 -0500

    Implemented the 32-bit Integer Operation benchmark; working on 32-bit Floating Point operation benchmark

--- main.cpp
+++ main.cpp
@@ -13,24 +13,40 @@ using namespace std;
 
 // Global Variable Declarations
 typedef chrono::high_resolution_clock Clock;
-float int_microseconds = NULL;
-float float_microseconds = NULL;
-float memory_microseconds = NULL;
+float int_microseconds       = NULL;
+float float_microseconds     = NULL;
+float memory_microseconds    = NULL;
 float hardDrive_microseconds = NULL;
 
 // Function Declaration
 void intOpBenchmark();
 void floatOpBenchmark();
+void memoryOpBenchmark();
+void hardDriveOpBenchmark();
 
 // Main
 int main() {
-    
+    intOpBenchmark();
+//    floatOpBenchmark();
+//    memoryOpBenchmark();
+//    hardDriveOpBenchmark();
     return 0;
 }
 
 // // // // // // // // // // // // // // // // // // // // // // // //
 void intOpBenchmark() {
-   
+    auto int_t1 = Clock::now();
+    const unsigned int n = 2;
+    
+    for(unsigned long long int i = 0; i < pow(10,11); i++)     {n+n;}
+    for(unsigned long long int i = 0; i < 5 * pow(10,10); i++) {n*n;}
+    for(unsigned long long int i = 0; i < 2 * pow(10,10); i++) {n/n;}
+    
+    auto int_t2 = Clock::now();
+
+    int_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(int_t2 - int_t1).count();
+
+    
 }
 // // // // // // // // // // // // // // // // // // // // // // // //
 
