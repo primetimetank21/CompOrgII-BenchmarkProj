774ee5000226085eebb6d183eb76a08a800ad326
Earl Tankard, Jr <theflashtank21@gmail.com>
2020-02-04 21:48:33 -0500

    First commit; Starting to set up Integer && Floating Point benchmarks

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  benchmarkMaker.cpp
+//
+//  Created by Earl Tankard, Jr. on 2/1/20.
+//  Copyright © 2020 Earl Tankard, Jr. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+#include <chrono>
+using namespace std;
+
+// Global Variable Declarations
+typedef chrono::high_resolution_clock Clock;
+float int_microseconds = NULL;
+float float_microseconds = NULL;
+float memory_microseconds = NULL;
+float hardDrive_microseconds = NULL;
+
+// Function Declaration
+void intOpBenchmark();
+void floatOpBenchmark();
+
+// Main
+int main() {
+    
+    return 0;
+}
+
+// // // // // // // // // // // // // // // // // // // // // // // //
+void intOpBenchmark() {
+   
+}
+// // // // // // // // // // // // // // // // // // // // // // // //
+
+void floatOpBenchmark() {
+   
+}
